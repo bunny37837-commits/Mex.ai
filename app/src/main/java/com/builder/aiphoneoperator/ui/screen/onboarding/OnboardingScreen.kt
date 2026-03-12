b302b8529e821163be0329d6225dfd7a77c664de
bootai134u-ux <bootai134u@gmail.com>
2026-03-12 20:36:18 +0530

    Fix P0 Material3 opt-in blockers

--- app/src/main/java/com/builder/aiphoneoperator/ui/screen/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/builder/aiphoneoperator/ui/screen/onboarding/OnboardingScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.builder.aiphoneoperator.ui.screen.onboarding
 
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
--- app/src/main/java/com/builder/aiphoneoperator/ui/screen/runningtask/RunningTaskScreen.kt
+++ app/src/main/java/com/builder/aiphoneoperator/ui/screen/runningtask/RunningTaskScreen.kt
@@ -1,6 +1,9 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.builder.aiphoneoperator.ui.screen.runningtask
 
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
